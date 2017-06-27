7ed024acf4810e7519f3d0c8142a570af23020b9
Toph Wells <toph.wells@gmail.com>
2017-06-28 00:18:22 +0200

    Week 2 work!

--- roguelike/game.js
+++ roguelike/game.js
@@ -1,15 +1,21 @@
 var Game = {
     display: null,
  
+	w: 60,
+	h: 40,
+ 
     init: function() {
+		
+		//perform canvas setup
 		var options = {
-			width: 60,
-			height: 40,
+			width: this.w,
+			height: this.h,
 			fontSize: 14,
-			forceSquareRatio:false
+			forceSquareRatio:true,
+			fontFamily: "Helvetica"
 		}
 		
-		container = document.getElementById("GameContainer");
+		var container = document.getElementById("GameContainer");
 		if (!("ROT" in window))
 		{
 			container.innerHTML = "Error: rot.js has not been loaded.";
@@ -25,8 +31,89 @@ var Game = {
         this.display = new ROT.Display(options);
         container.appendChild(this.display.getContainer());
 		
+		//setup player
+		var Player = function(x, y) {
+			this._x = x;
+			this._y = y;
+		}
+		Player.prototype.draw = function() {
+			Game.display.draw(this._x, this._y, "@", "#fff");
+		}
+		
+		Player.prototype.move = function(xDelta, yDelta) {
+			if (Math.abs(xDelta) + Math.abs(yDelta) != 1) //can't move more than one tile at a time
+				return;
+			
+			var newX = this._x + xDelta;
+			var newY = this._y + yDelta;
+			if (newX < 0 || newX >= Game.w || newY < 0 || newY >= Game.h) //can't move out of bounds
+				return;
+			if (Game.map[newX][newY]) //can't move into a wall (this implies moving *out* of a wall is fine, but that should never happen)
+				return;
+			this._x = newX;
+			this._y = newY;
+			
+		}
+		
+		//setup map generation
+		this.map = [];
+		for (var x = 0; x < this.w; x++)
+		{
+			this.map[x] = []; 
+		}
+		var mapCallback = function(x, y, value) {
+        this.map[x][y] = value;
+		}
+		this.generateMap = function() {
+			var cellular = new ROT.Map.Cellular(this.w, this.h,{topology: 4, born:[3,4], survive: [2,3,4]});
+			cellular.randomize(0.5);
+			cellular.create();
+			cellular.create();
+			cellular.create();
+			cellular.set(30, 20, false); //empty spot for the player to start in
+			cellular.connect();
+			cellular.serviceCallback(mapCallback.bind(this));
+		}
+		this.drawEverything = function() {
+			this.drawWholeMap();
+			this.player.draw();
+		}
+		this.drawWholeMap = function() {
+			for (var x = 0; x < this.w; x++)
+			{
+				for (var y = 0; y < this.h; y++)
+				{
+					if (this.map[x][y]) //wall
+					{
+						this.display.draw(x, y, " ", "#000", "#fff");
+					}
+					else //floor
+					{
+						this.display.draw(x, y, ".", "#fff", "#000");
+					}
+				}
+			}
+		}
 		
 		
-		this.display.drawText(20, 25, "%c{#00ff00}Congratulations!%c{} The game has started! But it's not doing anything yet.", 20);
+		//generate map
+		this.generateMap();
+		this.player = new Player(30,20);
+		this.drawEverything();
+
+		
+		//update loop
+		document.addEventListener("keydown", function(e) { //TODO: figure out what the best behaviour is if the user has something else selected
+			var code = e.keyCode;
+			if (code == ROT.VK_UP)
+				Game.player.move(0,-1);
+			if (code == ROT.VK_DOWN)
+				Game.player.move(0,1);
+			if (code == ROT.VK_LEFT)
+				Game.player.move(-1,0);
+			if (code == ROT.VK_RIGHT)
+				Game.player.move(1,0);
+			Game.drawEverything();
+		});
     }
 }
